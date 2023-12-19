a372fa76a81072e3718765115c8341c6c37e15f4
Keinazj <jafari.keinaz@gmail.com>
2023-12-19 23:54:17 +0330

    get input from a file and from the user at the time

--- Assembler.py
+++ Assembler.py
@@ -643,192 +643,221 @@ def pop(register):
     return str(opcode)
 
 #####################################################
+def  main2():
+    inputFile = open("input_assembly.txt", "r")
+    outputFile = open("output_assembly.txt", "w")
 
-inputFile = open("input_assembly.txt", "r")
-outputFile = open("output_assembly.txt", "w")
+    op_code = [0] * 8
+    mod_rm = [0] * 8
+    address = 0x0000000000000000
+    machine_code_lines = []
+    opcode_str = ""
 
-op_code = [0] * 8
-mod_rm = [0] * 8
-address = 0x0000000000000000
-machine_code_lines = []
-opcode_str = ""
+    labels = {}
+    jumps = []
+    idx = -1
 
-labels = {}
-jumps = []
-idx = -1
-
-for line in inputFile:
-    idx+=1
-    line = line.rstrip()
-    if line == '':
-        continue
-    
-    inp = line.split(' ')
-    if inp[0].endswith(':'):
-        idx-=1
-        label = inp[0][:-1]  # Remove :
-        labels[label] = address  # Store the label and its address in the dictionary
-        continue
-    else:
-        inp[1] = inp[1].replace(",", "")
-        output_line = ""
-        formatted_address = f'0x{address:016x}'
-        output_line += formatted_address + '\t'
-
-    
-    if inp[0].lower() == 'jmp':
+    for line in inputFile:
+        idx+=1
+        line = line.rstrip()
+        if line == '':
+            continue
         
-        if inp[1] in labels:  # Check if the jump target is a defined label
-            jump_target_address = labels[inp[1]]
-            if jump_target_address < address:  # Check if it's a backward jump
-              
-                jump_offset = jump_target_address - (address + 2)  # Calculate the jump offset
-                if jump_offset < 0:
-                     jump_offset = 256 + jump_offset  
-                output_line += f'eb {jump_offset:02x}'  # backwardJump output
-            address+=2
+        inp = line.split(' ')
+        if inp[0].endswith(':'):
+            idx-=1
+            label = inp[0][:-1]  # Remove :
+            labels[label] = address  # Store the label and its address in the dictionary
+            continue
+        else:
+            inp[1] = inp[1].replace(",", "")
+            output_line = ""
+            formatted_address = f'0x{address:016x}'
+            output_line += formatted_address + '\t'
+
+        
+        if inp[0].lower() == 'jmp':
             
-        else:  # It's a forward jump
-        
-            output_line += 'eb 00'  # a placeholder for the forward jump machine code
-            jumps.append((idx,formatted_address, inp[1]))  # Store the jump line and label
-            machine_code_lines.append(output_line)
-            address+=2 
-            continue   
-    
-    elif inp[0].lower() =="add":
-        op_code = [0] * 8
-        if inp[1] in sixteen_bit_registers:
-            address+=3
-        else:
-            address=address + 2
-        output_line+=(add(inp,op_code,mod_rm))
-        output_text = '\n'.join(machine_code_lines)
-        
-        
-    elif inp[0].lower() =="sub":
-        op_code = [0] * 8
-        if inp[1] in sixteen_bit_registers:
-            address+=3
-        else:
-            address+=2
-            
-        output_line+=(sub(inp,op_code,mod_rm))
-        output_text = '\n'.join(machine_code_lines)       
-        
-    elif inp[0] =="And":
-        op_code = [0] * 8
-        if inp[1] in sixteen_bit_registers:
-            address+=3
-        else:
-            address=address + 2
-        output_line+=(And(inp,op_code,mod_rm))
-        output_text = '\n'.join(machine_code_lines)
-        
-    elif inp[0] =="Or":
-        op_code = [0] * 8
-        if inp[1] in sixteen_bit_registers:
-            address+=3
-        else:
-            address=address + 2
-       
-        output_line+=(Or(inp,op_code,mod_rm))
-        output_text = '\n'.join(machine_code_lines)
-        
-    elif inp[0].lower() =="xor":
-        op_code = [0] * 8
-    
-        if inp[1] in sixteen_bit_registers:
-            address+=3
-        else:
-            address=address + 2
-        output_line+=(Xor(inp,op_code,mod_rm))
-        output_text = '\n'.join(machine_code_lines)
-        
-    elif inp[0].lower() =="inc":
-        op_code = [0] * 8
-        
-        if inp[1] in sixteen_bit_registers:
-            address+=2
-        else:
-            address=address + 1
-        if inp[1] in sixteen_bit_registers:
-            output_line+=('66 ')
-            
-        output_line+=(inc(inp[1]))
-        output_text = '\n'.join(machine_code_lines)
-        
-    elif inp[0].lower() =="dec":
-        op_code = [0] * 8
-
-        if inp[1] in sixteen_bit_registers:
-            address+=2
-        else:
-            address=address + 1
-        if inp[1] in sixteen_bit_registers:
-            output_line+=('66 ')
-        output_line+=(dec(inp[1]))
-        output_text = '\n'.join(machine_code_lines)
-                    
-    elif inp[0].lower() =="pop":
-        op_code = [0] * 8
-        if inp[1] in thirty_two_bit_registers:
-            address+=2
-        else:
-            address=address + 1
-        if inp[1] in sixteen_bit_registers:
-            output_line+=('66 ')
-        output_line+=(pop(inp[1]))
-        output_text = '\n'.join(machine_code_lines)
-                
-    elif inp[0].lower() =="push":
-        value_avalie = inp[1]
-        op_code = [0] * 8
-        if inp[1] in sixteen_bit_registers:
-            address+=2
-        elif inp[1] in thirty_two_bit_registers:
-            address=address + 1
-        else:
-            if (inp[1][-1] >'9' or inp[1][-1] <'0'):
-                inp[1] = inp[1][:-1]
-            if int(inp[1],16)<= 255:
+            if inp[1] in labels:  # Check if the jump target is a defined label
+                jump_target_address = labels[inp[1]]
+                if jump_target_address < address:  # Check if it's a backward jump
+                  
+                    jump_offset = jump_target_address - (address + 2)  # Calculate the jump offset
+                    if jump_offset < 0:
+                         jump_offset = 256 + jump_offset  
+                    output_line += f'eb {jump_offset:02x}'  # backwardJump output
                 address+=2
-            elif int(inp[1],16) >255:
-                address+=5
-               
-        if inp[1] in sixteen_bit_registers:
-            output_line+=('66 ')
-        inp[1] = value_avalie
-        output_line+=str(push(inp[1]))
+                
+            else:  # It's a forward jump
+            
+                output_line += 'eb 00'  # a placeholder for the forward jump machine code
+                jumps.append((idx,formatted_address, inp[1]))  # Store the jump line and label
+                machine_code_lines.append(output_line)
+                address+=2 
+                continue   
+        
+        elif inp[0].lower() =="add":
+            op_code = [0] * 8
+            if inp[1] in sixteen_bit_registers:
+                address+=3
+            else:
+                address=address + 2
+            output_line+=(add(inp,op_code,mod_rm))
+            output_text = '\n'.join(machine_code_lines)
+            
+            
+        elif inp[0].lower() =="sub":
+            op_code = [0] * 8
+            if inp[1] in sixteen_bit_registers:
+                address+=3
+            else:
+                address+=2
+                
+            output_line+=(sub(inp,op_code,mod_rm))
+            output_text = '\n'.join(machine_code_lines)       
+            
+        elif inp[0] =="And":
+            op_code = [0] * 8
+            if inp[1] in sixteen_bit_registers:
+                address+=3
+            else:
+                address=address + 2
+            output_line+=(And(inp,op_code,mod_rm))
+            output_text = '\n'.join(machine_code_lines)
+            
+        elif inp[0] =="Or":
+            op_code = [0] * 8
+            if inp[1] in sixteen_bit_registers:
+                address+=3
+            else:
+                address=address + 2
+           
+            output_line+=(Or(inp,op_code,mod_rm))
+            output_text = '\n'.join(machine_code_lines)
+            
+        elif inp[0].lower() =="xor":
+            op_code = [0] * 8
+        
+            if inp[1] in sixteen_bit_registers:
+                address+=3
+            else:
+                address=address + 2
+            output_line+=(Xor(inp,op_code,mod_rm))
+            output_text = '\n'.join(machine_code_lines)
+            
+        elif inp[0].lower() =="inc":
+            op_code = [0] * 8
+            
+            if inp[1] in sixteen_bit_registers:
+                address+=2
+            else:
+                address=address + 1
+            if inp[1] in sixteen_bit_registers:
+                output_line+=('66 ')
+                
+            output_line+=(inc(inp[1]))
+            output_text = '\n'.join(machine_code_lines)
+            
+        elif inp[0].lower() =="dec":
+            op_code = [0] * 8
+
+            if inp[1] in sixteen_bit_registers:
+                address+=2
+            else:
+                address=address + 1
+            if inp[1] in sixteen_bit_registers:
+                output_line+=('66 ')
+            output_line+=(dec(inp[1]))
+            output_text = '\n'.join(machine_code_lines)
+                        
+        elif inp[0].lower() =="pop":
+            op_code = [0] * 8
+            if inp[1] in thirty_two_bit_registers:
+                address+=2
+            else:
+                address=address + 1
+            if inp[1] in sixteen_bit_registers:
+                output_line+=('66 ')
+            output_line+=(pop(inp[1]))
+            output_text = '\n'.join(machine_code_lines)
+                    
+        elif inp[0].lower() =="push":
+            value_avalie = inp[1]
+            op_code = [0] * 8
+            if inp[1] in sixteen_bit_registers:
+                address+=2
+            elif inp[1] in thirty_two_bit_registers:
+                address=address + 1
+            else:
+                if (inp[1][-1] >'9' or inp[1][-1] <'0'):
+                    inp[1] = inp[1][:-1]
+                if int(inp[1],16)<= 255:
+                    address+=2
+                elif int(inp[1],16) >255:
+                    address+=5
+                   
+            if inp[1] in sixteen_bit_registers:
+                output_line+=('66 ')
+            inp[1] = value_avalie
+            output_line+=str(push(inp[1]))
+            output_text = '\n'.join(machine_code_lines)
+                  
+        machine_code_lines.append(output_line)
+        
+    for line_index , jump_line, label in jumps:
+        jump_target_address = labels[label]
+       # line_index = int(jump_line, 16)
+        jump_offset = jump_target_address - (int(jump_line, 16)+2) # to jump to the next line of the jmp instruction 
+
+        # Two's complement
+        jump_offset = (~(-jump_offset) + 1) & 0xFF
+        formatted_hex_offset = f'{jump_offset:02x}' 
+      
+        # Replace the placeholder in the specific line and update the list
+        machine_code_lines[line_index] = machine_code_lines[line_index].replace('eb 00', f'eb {formatted_hex_offset}')
+        #print(machine_code_lines)
+
+    # create the output text by joining all the elements from 'machine_code_lines'
         output_text = '\n'.join(machine_code_lines)
-              
-    machine_code_lines.append(output_line)
-    
-for line_index , jump_line, label in jumps:
-    jump_target_address = labels[label]
-   # line_index = int(jump_line, 16)
-    jump_offset = jump_target_address - (int(jump_line, 16)+2) # to jump to the next line of the jmp instruction 
+        
+    for line in machine_code_lines:
+        outputFile.write(output_text)
+        print(line)
 
-    # Two's complement
-    jump_offset = (~(-jump_offset) + 1) & 0xFF
-    formatted_hex_offset = f'{jump_offset:02x}' 
-  
-    # Replace the placeholder in the specific line and update the list
-    machine_code_lines[line_index] = machine_code_lines[line_index].replace('eb 00', f'eb {formatted_hex_offset}')
-    #print(machine_code_lines)
+def main():
+    get_input = int(input("how do you want to give the input? type 1 for typing your input , type 2 for giving a File\n"))
+    if (get_input==1):
+        print("enter your input:")
+        while True:
+            instruction = input()
+            if instruction =='':
+                return
+            inputFile = open("input_assembly.txt","w")
+            print(instruction,file=inputFile)
+            inputFile.close()
+            main2()
+            outputFile = open("output_assembly.txt","r")
 
-# create the output text by joining all the elements from 'machine_code_lines'
-    output_text = '\n'.join(machine_code_lines)
-    
-for line in machine_code_lines:
-    outputFile.write(output_text)
-    print(line)
-
-outputFile.close() 
-inputFile.close()        
+            outputFile.close()
+    elif(get_input==2):
+        main2()
+    else:
+        print("invalid input")
+        
+main()
+                    
+                
+                    
+                    
+                    
+                    
+                    
+                    
 
      
     
+
+
 
 
 
